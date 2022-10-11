8e3c7ca0b4a20fe6350f82afb1a3d6871f6d81ed
Sean Murtagh <purelolage@googlemail.com>
2022-10-11 23:55:46 +0100

    removed theme swapper, night them default

--- script.js
+++ script.js
@@ -25,30 +25,30 @@ speechSynthesis.addEventListener('voiceschanged', populateVoices)
 
 // THEME SWITCHER //
 
-theme = localStorage.getItem('theme') || 'light';
+// theme = localStorage.getItem('theme') || 'light';
 
-const loadTheme = (theme) => {
+// const loadTheme = (theme) => {
 
-    var link = document.createElement('link');
-    link.href = `${theme}_theme.css`;
-    link.rel = 'stylesheet';
-    link.type = 'text/css';
-    link.setAttribute('id', 'themesheet');
+//     var link = document.createElement('link');
+//     link.href = `${theme}_theme.css`;
+//     link.rel = 'stylesheet';
+//     link.type = 'text/css';
+//     link.setAttribute('id', 'themesheet');
     
-    document.body.appendChild(link);
-}
+//     document.body.appendChild(link);
+// }
 
-loadTheme(theme);
-if (theme == 'dark') {
-    checkbox = document.getElementById('theme');
-    checkbox.checked = true;
-}
+// loadTheme(theme);
+// if (theme == 'dark') {
+//     checkbox = document.getElementById('theme');
+//     checkbox.checked = true;
+// }
 
-const toggleTheme = () => {
-    theme == 'light' ? theme = 'dark' : theme = 'light';
-    localStorage.setItem('theme', theme);
-    var oldcss = document.getElementById('themesheet');
-    oldcss.parentNode.removeChild(oldcss); 
-    loadTheme(theme)
-}
+// const toggleTheme = () => {
+//     theme == 'light' ? theme = 'dark' : theme = 'light';
+//     localStorage.setItem('theme', theme);
+//     var oldcss = document.getElementById('themesheet');
+//     oldcss.parentNode.removeChild(oldcss); 
+//     loadTheme(theme)
+// }
 
